70efcd9a68134ab8bf61be981ce9bbe2e2ed7e7f
Connor <cmd6824@psu.edu>
2024-02-18 16:48:46 -0500

    counter app implemented

--- src/counter-app.js
+++ src/counter-app.js
@@ -13,8 +13,8 @@ export class CounterApp extends LitElement {
 
   constructor() {
     super();
-    this.counter = "0";
-    this.min = "0";
+    this.counter = "5";
+    this.min = "1";
     this.max = "10";
   }
 
@@ -32,50 +32,105 @@ export class CounterApp extends LitElement {
       :host([counter="21"]) .counterapp {
         color: green;
       }
-      
+
       .counterapp {
         display: block;
         margin: 0 auto;
         background-color: white;
         border: 2px solid black;
-        max-width: 50px;
+        width: 100px;
         text-align: center;
-        padding: 12px;
-        font-size: 30px;
+        padding: 4px;
+        font-size: 20px;
       }
 
-      .btnadd {
-        display: inline;
-        margin: 0 auto;
-      }
-
+      .btnadd,
       .btnsubtract {
-        display: inline;
+        display: inline-flex;
         margin: 0 auto;
+        text-align: center;
+        font-size: 20px;
       }
+
+      .button-wrap {
+        padding-left: 25px;
+      }
+
+      .btnadd:hover,
+      .btnadd:focus-within {
+        background-color: green;
+      }
+
+      .btnsubtract:hover,
+      .btnsubtract:focus-within {
+        background-color: red;
+      }
+
       `
     }
 
-
-
   render() {
     return html`
+    <confetti-container id="confetti">
     <div class="counterapp">
-      <span>${this.counter}</span>
+      <h1 class="counter">${this.counter}</h1>
     </div>
     <div class="button-wrap">
-      <button class="btnadd">+</button>
-      <button class="btnsubtract">-</button>
+      <button class="btnadd" @click="${this.add}">+</button>
+      <button class="btnsubtract" @click="${this.subtract}">-</button>
     </div>
-
+    </confetti-container>
     `;
+
   }
+
+  add() {
+    if (this.counter < this.max && this.counter >= this.min) {
+      this.counter++;
+    }
+  }
+
+  subtract() {
+    if (this.counter <= this.max && this.counter > this.min) {
+      this.counter--;
+    }
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('counter')) {
+      // do your testing of the value and make it rain by calling makeItRain
+      if (this.counter == 21) {
+        this.makeItRain();
+      }
+    }
+  }
+  
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
+
 
   static get properties() {
     return {
-      counter: { type: Number, relfect: true },
-      min: { type: String },
-      max: { type: String },
+      counter: { type: Number, reflect: true },
+      min: { type: Number, reflect: true },
+      max: { type: Number, reflect: true },
     };
   }
 }
